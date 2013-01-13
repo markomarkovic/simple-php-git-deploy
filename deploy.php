39b31a4657a454741ed8d07b4a1658a2a982a845
Marko Marković <okram@civokram.com>
2013-01-13 16:55:59 +0100

    Removing indentation

--- deploy.php
+++ deploy.php
@@ -1,34 +1,34 @@
 <?php
-	/**
-	 * GIT DEPLOYMENT SCRIPT
-	 *
-	 * Used for automatically deploying websites via github or bitbucket, more deets here:
-	 *
-	 *		https://gist.github.com/1809044
-	 */
+/**
+ * GIT DEPLOYMENT SCRIPT
+ *
+ * Used for automatically deploying websites via github or bitbucket, more deets here:
+ *
+ *		https://gist.github.com/1809044
+ */
 
-	// The commands
-	$commands = array(
-		'echo $PWD',
-		'whoami',
-		'git pull',
-		'git status',
-		'git submodule sync',
-		'git submodule update',
-		'git submodule status',
-	);
+// The commands
+$commands = array(
+	'echo $PWD',
+	'whoami',
+	'git pull',
+	'git status',
+	'git submodule sync',
+	'git submodule update',
+	'git submodule status',
+);
 
-	// Run the commands for output
-	$output = '';
-	foreach($commands AS $command){
-		// Run it
-		$tmp = shell_exec($command);
-		// Output
-		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
-		$output .= htmlentities(trim($tmp)) . "\n";
-	}
+// Run the commands for output
+$output = '';
+foreach($commands AS $command){
+	// Run it
+	$tmp = shell_exec($command);
+	// Output
+	$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+	$output .= htmlentities(trim($tmp)) . "\n";
+}
 
-	// Make it pretty for manual user access (and why not?)
+// Make it pretty for manual user access (and why not?)
 ?>
 <!DOCTYPE HTML>
 <html lang="en-US">
