4b7727f23e5127f43a92f5016b0318bb1bd95e0a
Marko Marković <okram@civokram.com>
2013-02-19 15:24:59 +0100

    Added more comments for configuration options

--- deploy.php
+++ deploy.php
@@ -14,23 +14,31 @@
  * Protect the script from unauthorized access by using a secret string.
  * If it's not present in the access URL as a GET variable named `sat`
  * e.g. deploy.php?sat=Bett...s the script is going to fail.
+ *
+ * @var string
  */
 define('SECRET_ACCESS_TOKEN', 'BetterChangeMeNowOrSufferTheConsequences');
 
 /**
  * The address of the remote GIT repository that contains the code we're
- * updating
+ * updating.
+ *
+ * @var string
  */
 define('REMOTE_REPOSITORY', 'https://github.com/markomarkovic/simple-php-git-deploy.git');
 
 /**
  * Which branch are we going to use for deployment.
+ *
+ * @var string
  */
 define('BRANCH', 'master');
 
 /**
  * This is where the code resides on the local machine.
  * Don't forget the trailing slash!
+ *
+ * @var string Full path including the trailing slash
  */
 define('TARGET_DIR', '/tmp/simple-php-git-deploy/');
 
@@ -41,6 +49,8 @@ define('TARGET_DIR', '/tmp/simple-php-git-deploy/');
  * !!! WARNING !!! This can lead to a serious loss of data if you're not
  * careful. All files that are not in the repository are going to be deleted,
  * except the ones defined in EXCLUDE section! BE CAREFUL!
+ *
+ * @var boolean
  */
 define('DELETE_FILES', false);
 
@@ -49,6 +59,8 @@ define('DELETE_FILES', false);
  * Normally, these are the directories containing files that are not part of
  * code base, for example user uploads or server-specific configuration files.
  * Use rsync exclude pattern syntax for each element.
+ *
+ * @var serialized array of strings
  */
 define('EXCLUDE', serialize(array(
 	'.git',
@@ -58,16 +70,22 @@ define('EXCLUDE', serialize(array(
 
 /**
  * Temporary directory we'll use to stage the code before the update.
+ *
+ * @var string Full path including the trailing slash
  */
 define('TMP_DIR', '/tmp/spgd-'.md5(REMOTE_REPOSITORY).'-'.time().'/');
 
 /**
  * Output the version of the deployed code.
+ *
+ * @var string Full path to the file name
  */
 define('VERSION_FILE', TMP_DIR.'DEPLOYED_VERSION.txt');
 
 /**
- * Time limit for each command, in seconds
+ * Time limit for each command.
+ *
+ * @var int Time in seconds
  */
 define('TIME_LIMIT', 30);
 
@@ -197,6 +215,8 @@ $commands[] = sprintf(
 	, (DELETE_FILES) ? '--delete-after' : ''
 	, $exclude
 );
+
+// Post-Deployment
 
 // Remove the TMP_DIR
 $commands['cleanup'] = sprintf(
