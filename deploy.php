dec2d9416c07f468c331c60d7630ba2a14a58279
Marko Marković <okram@civokram.com>
2014-02-23 13:05:28 +0100

    Fixes #22 Proper HTTP response when access is denied.

--- deploy.php
+++ deploy.php
@@ -141,6 +141,10 @@ if (!defined('COMPOSER_OPTIONS')) define('COMPOSER_OPTIONS', '--no-dev');
 
 // ===========================================[ Configuration end ]===
 
+// If there's authorization error, set the correct HTTP header.
+if (!isset($_GET['sat']) || $_GET['sat'] !== SECRET_ACCESS_TOKEN || SECRET_ACCESS_TOKEN === 'BetterChangeMeNowOrSufferTheConsequences') {
+	header('HTTP/1.0 403 Forbidden');
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
